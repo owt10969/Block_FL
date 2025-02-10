49d43882e01651156fbda9732f8be0d804c4b4c6
Yumemin <410635034@gms.ndhu.edu.tw>
2025-02-10 21:45:19 +0800

    feat: temp

--- internal/app/container/container.go
+++ internal/app/container/container.go
@@ -5,16 +5,19 @@ import (
 	"Go-FederatedLearning/internal/app/builder"
 	imagehandler "Go-FederatedLearning/internal/handler/image"
 	printhandler "Go-FederatedLearning/internal/handler/print"
+	globalhanlder "Go-FederatedLearning/internal/handler/global"
 )
 
 type HandlerContainer struct {
 	PrintHandler *printhandler.Handler
 	ImageHandler *imagehandler.Handler
+	GlobalHandler *globalhandler.Handler
 }
 
 func NewHandlerContainer(service *builder.ServiceContainer) *HandlerContainer {
 	return &HandlerContainer{
 		PrintHandler: printhandler.NewHandler(service.PrintService),
 		ImageHandler: imagehandler.NewHandler(service.ImageService),
+		GlobalHandler: globalhandler.NewHander(service.GlobalService),
 	}
 }
